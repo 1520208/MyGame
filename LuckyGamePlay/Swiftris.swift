28c6834b1266486442966a2e47eb50733f889b0d
1520208 <1520208@stud.hs-mannheim.de>
2017-05-24 09:31:08 +0200

    swifts changed, droping shapes and endGame()

--- LuckyGamePlay/Swiftris.swift
+++ LuckyGamePlay/Swiftris.swift
@@ -14,10 +14,31 @@ let StartingRow = 0
 let PreviewColumn = 12
 let PreviewRow = 1
 
+protocol SwiftrisDelegate {
+    // Invoked when the current round of Swiftris ends
+    func gameDidEnd(swiftris: Swiftris)
+    
+    // Invoked after a new game has begun
+    func gameDidBegin(swiftris: Swiftris)
+    
+    // Invoked when the falling shape has become part of the game board
+    func gameShapeDidLand(swiftris: Swiftris)
+    
+    // Invoked when the falling shape has changed its location
+    func gameShapeDidMove(swiftris: Swiftris)
+    
+    // Invoked when the falling shape has changed its location after being dropped
+    func gameShapeDidDrop(swiftris: Swiftris)
+    
+    // Invoked when the game has reached a new level
+    func gameDidLevelUp(swiftris: Swiftris)
+}
+
 class Swiftris{
     var blockArray:Array2D<Block>
     var nextShape:Shape?
     var fallingShape:Shape?
+    var delegate:SwiftrisDelegate?
     
     init(){
         fallingShape = nil
@@ -30,6 +51,7 @@ class Swiftris{
         if (nextShape == nil) {
             nextShape = Shape.random(startingColumn:PreviewColumn, startingRow: PreviewRow)
         }
+        delegate?.gameDidBegin(swiftris: self)
     }
     
     //method which assigns the preview shape as nextShape, fallingShape is the moving Tetromino
@@ -40,7 +62,130 @@ class Swiftris{
         fallingShape = nextShape
         nextShape = Shape.random(startingColumn:PreviewColumn, startingRow:PreviewRow)
         fallingShape?.moveTo(column:StartingColumn, row:StartingRow)
+    
+        //game will end if there is no location for a shape anymore
+        guard detectIllegalPlacement() == false else {
+            nextShape = fallingShape
+            nextShape!.moveTo(column: PreviewColumn, row: PreviewRow)
+            endGame()
+            return (nil, nil)
+        }
+        
         return (fallingShape, nextShape)
         
     }
+    
+    func detectIllegalPlacement() -> Bool {
+        guard let shape = fallingShape else {
+            return false
+        }
+        for block in shape.blocks {
+            if block.column < 0 || block.column >= NumColumns
+                || block.row < 0 || block.row >= NumRows {
+                return true
+            } else if blockArray[block.column, block.row] != nil {
+                return true
+            }
+        }
+        return false
+    }
+    
+    
+    func letShapeFall() {
+        guard let shape = fallingShape else {
+            return
+        }
+        shape.lowerShapeByOneRow()
+        if detectIllegalPlacement() {
+            shape.raiseShapeByOneRow()
+            if detectIllegalPlacement() {
+                endGame()
+            } else {
+                settleShape()
+            }
+        } else {
+            delegate?.gameShapeDidMove(swiftris: self)
+            if detectTouch() {
+                settleShape()
+            }
+        }
+    }
+
+    func rotateShape() {
+        guard let shape = fallingShape else {
+            return
+        }
+        shape.rotateClockwise()
+        guard detectIllegalPlacement() == false else {
+            shape.rotateCounterClockwise()
+            return
+        }
+        delegate?.gameShapeDidMove(swiftris:self)
+    }
+    
+    func moveShapeLeft() {
+        guard let shape = fallingShape else {
+            return
+        }
+        shape.shiftLeftByOneColumn()
+        guard detectIllegalPlacement() == false else {
+            shape.shiftRightByOneColumn()
+            return
+        }
+        delegate?.gameShapeDidMove(swiftris:self)
+    }
+    
+    func moveShapeRight() {
+        guard let shape = fallingShape else {
+            return
+        }
+        shape.shiftRightByOneColumn()
+        guard detectIllegalPlacement() == false else {
+            shape.shiftLeftByOneColumn()
+            return
+        }
+        delegate?.gameShapeDidMove(swiftris: self)
+    }
+    
+    func settleShape() {
+        guard let shape = fallingShape else {
+            return
+        }
+        for block in shape.blocks {
+            blockArray[block.column, block.row] = block
+        }
+        fallingShape = nil
+        delegate?.gameShapeDidLand(swiftris: self)
+    }
+    
+    // #9
+    func detectTouch() -> Bool {
+        guard let shape = fallingShape else {
+            return false
+        }
+        for bottomBlock in shape.bottomBlocks {
+            if bottomBlock.row == NumRows - 1
+                || blockArray[bottomBlock.column, bottomBlock.row + 1] != nil {
+                return true
+            }
+        }
+        return false
+    }
+    
+    func endGame() {
+        delegate?.gameDidEnd(swiftris: self)
+    }
+    
+    func dropShape() {
+        guard let shape = fallingShape else {
+            return
+        }
+        while detectIllegalPlacement() == false {
+            shape.lowerShapeByOneRow()
+        }
+        shape.raiseShapeByOneRow()
+        delegate?.gameShapeDidDrop(swiftris: self)
+    }
+    
+    
 }
